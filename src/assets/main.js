49b85f4a47851336d758898f673630c10573c70e
Tony DeMillard <ademillard@gmail.com>
2018-01-30 16:17:52 -0700

    Markup changes

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/4448446.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      console.log(response.courses.completed);     
+
+      response.courses.completed.forEach(element => {
+        $('#badges').append('<div class=\'course\'><h3>' + element.title +
+                            '<img src=\'' + element.badge + '\' />' +
+                            '<a href=\'' + element.url + '\' target=\'_blank\'' + 
+                            ' class=\'btn btn-primary\'> See Course</a>' +
+                            '</div>');
+      });
+    }
+  }); 
 
 });
